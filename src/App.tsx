a38b72c6c16131fe2e33103b7cf6288e583722a8
David Sturesson <sturessondavid@gmail.com>
2021-02-11 17:55:11 +0100

    Showing bc value

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,63 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from 'react';
+import { useQuery } from 'react-query';
+// Styles
+import { Wrapper } from './styles/App.styles';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+type BitcoinData = {
+	'15m': number;
+	buy: number;
+	last: number;
+	sell: number;
+	symbol: string;
+};
+
+type Currencies = {
+	[key: string]: BitcoinData;
+};
+
+const getBCData = async (): Promise<Currencies> => {
+	return await (await fetch('https://blockchain.info/ticker')).json();
+};
+
+const INTERVAL_TIME = 20000; // 20s
+
+const App = () => {
+	const [currency, setCurrency] = useState('USD');
+	const { data, isLoading, error, refetch } = useQuery<Currencies>(
+		'bc-data',
+		getBCData,
+		{
+			refetchInterval: INTERVAL_TIME,
+		},
+	);
+
+	const handleCurrencySelection = (e: any) => {
+		setCurrency(e.currentTarget.value);
+	};
+
+	if (isLoading) return <div>Loading...</div>;
+	if (error) return <div>Something went wrong...</div>;
+	return (
+		<Wrapper>
+			<>
+				<h2>Bitcoin Price</h2>
+				<select value={currency} onChange={handleCurrencySelection}>
+					{data &&
+						Object.keys(data).map(currency => (
+							<option key={currency} value={currency}>
+								{currency}
+							</option>
+						))}
+				</select>
+				<div>
+					<h2>
+						{data && data[currency].last}
+						{data && data[currency].symbol}
+					</h2>
+				</div>
+			</>
+		</Wrapper>
+	);
+};
 
 export default App;
